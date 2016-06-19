58e7bcbee87db0956d9b963aaa6a6aa0b565e48e
Hugo Dzin <humodz@hotmail.com>
2016-06-19 19:25:31 +0900

    pong, sfx

--- pong/Ball.java
+++ pong/Ball.java
@@ -7,13 +7,18 @@ public class Ball extends Actor
     private int speed = 4;
     private int trueRotation = 0;
     
-    public Ball()
-    {
-        // Create a ball with the default values above
-    }
+    // Sound effects
+    private GreenfootSound hitPaddle = new GreenfootSound("tone1.mp3");
+    private GreenfootSound hitWall = new GreenfootSound("twoTone2.mp3");
+    private GreenfootSound score = new GreenfootSound("threeTone1.mp3");
+    
     
     public Ball(int spd, int angle)
     {
+        hitPaddle.setVolume(50);
+        hitWall.setVolume(50);
+        score.setVolume(50);
+        
         speed = spd;
         trueRotation = angle;
     }
@@ -35,6 +40,8 @@ public class Ball extends Actor
         // Bounce off the paddles
         if(paddle != null)
         {
+            sound(hitPaddle);
+
             // Become faster, if not already too fast
             if(speed < 15)
                 speed++;
@@ -47,7 +54,7 @@ public class Ball extends Actor
             angle = getRotation();
             int cap = 30;
             
-            // There are four possible directions:
+            // There are four directions we have to take into account
             if(90 - cap < angle && angle <= 90) // Down-right |\
                 angle = 90 - cap;
             else if(90 < angle && angle < 90 + cap) // Down-left /|
@@ -61,16 +68,28 @@ public class Ball extends Actor
         }
         // Touching left wall, player 2 scores
         else if(getX() - radius <= 0)
-            pong.goal(2);
+        {
+            sound(score);
+            pong.goal(2);   
+        }
         // Touching right wall, player 1 scores
         else if(getX() + radius >= getWorld().getWidth() - 1)
+        {
+            sound(score);
             pong.goal(1);
+        }
         // Bounce off the top wall
         else if(angle > 180 && getY() - radius <= 0)
+        {
+            sound(hitWall);
             setRotation(-angle);
+        }
         // Bounce of the bottom wall
         else if(angle < 180 && getY() + radius >= getWorld().getHeight() - 1)
-            setRotation(-angle);
+        {
+            sound(hitWall);
+            setRotation(-angle);   
+        }
                 
         move(speed);
         
@@ -78,5 +97,12 @@ public class Ball extends Actor
         trueRotation = getRotation();
         // Set the actor's rotation to 0 so the image doesn't look rotated
         setRotation(0);
+    }
+    
+    private void sound(GreenfootSound s)
+    {
+        if(s.isPlaying())
+            s.stop();
+        s.play();
     }
 }
